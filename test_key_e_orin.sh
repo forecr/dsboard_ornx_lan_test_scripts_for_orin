053c8602fcb21a239e2b2a1fbf38affac04bfb17
mdeniz0 <mehmetdeniz@mistelektronik.com>
2023-04-11 12:16:49 +0300

    Initial commit

---
+++ test_key_e_orin.sh
@@ -0,0 +1,14 @@
+sudo echo 331 > /sys/class/gpio/export
+sudo echo 391 > /sys/class/gpio/export
+
+sudo echo high > /sys/class/gpio/PCC.03/direction
+
+trap disable_keye INT
+disable_keye() {
+	sudo echo low > /sys/class/gpio/PCC.03/direction
+	sudo echo 331 > /sys/class/gpio/unexport
+	sudo echo 391 > /sys/class/gpio/unexport
+}
+
+watch -n 0.1 lsusb
+

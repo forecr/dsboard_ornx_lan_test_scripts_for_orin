212aa2940b207060e29d517b59b7ba117dd36c64
mdeniz0 <mehmetdeniz@mistelektronik.com>
2023-11-13 15:05:00 +0300

    M.2 Key-E test commands updated

--- test_key_e_orin.sh
+++ test_key_e_orin.sh
@@ -1,6 +1,7 @@
 sudo echo 331 > /sys/class/gpio/export
 sudo echo 391 > /sys/class/gpio/export
 
+sudo echo low > /sys/class/gpio/PH.00/direction
 sudo echo high > /sys/class/gpio/PCC.03/direction
 
 trap disable_keye INT
